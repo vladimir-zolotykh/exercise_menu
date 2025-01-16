769b1550a4d19cdfdbab9ebf80c73149726794d0
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-16 09:06:43 +0200

    Add Label with an image

--- start.py
+++ start.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-
+import os
+import tkinter as tk
+from PIL import Image, ImageTk
 
 if __name__ == '__main__':
-    print('start exercise_menu')
+    root = tk.Tk()
+    image = Image.open(os.path.expanduser(
+        "~/Downloads/Screenshot_20250115-191509_Gallery.jpg"))
+    photo = ImageTk.PhotoImage(image)
+    label = tk.Label(root, image=photo)
+    label.pack()
+    root.mainloop()
