d84ea083eb5231c690218db962c87ae71e3ec626
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-20 09:33:06 +0200

    Implement ScrolledCanvas (template)
    Has bugs

--- scrolledcanvas.py
+++ scrolledcanvas.py
@@ -6,4 +6,18 @@ import tkinter as tk
 
 class ScrolledCanvas(tk.Canvas):
     def __init__(self, owner, **kwargs):
-        super().__init__(owner, **kwargs)
+        box = tk.Frame(owner)
+        box.grid(column=0, row=0, sticky=tk.NSEW)
+        hbar = tk.Scrollbar(box, orient=tk.HORIZONTAL, command=self.xview)
+        hbar.grid(column=0, row=1, sticky=tk.EW)
+        vbar = tk.Scrollbar(box, orient=tk.VERTICAL, command=self.yview)
+        vbar.grid(column=1, row=0, sticky=tk.NS)
+        kwargs.update({'xscrollcommand': hbar.set, 'yscrollcommand': vbar.set})
+        super().__init__(box, **kwargs)
+        self.grid(column=0, row=0, sticky=tk.NSEW)
+        for meth in dir(box):
+            attr = getattr(box, meth)
+            if meth.startswith('grid') and callable(attr):
+                setattr(self, meth, attr)
+        
+                      
