93ed7c2e0f4871c446b3cd705c35cd1723ac8fdf
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-22 07:32:45 +0200

    Add RegisterCash(Register) class

--- register.py
+++ register.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from collections import namedtuple
 import tkinter as tk
 from PIL import Image, ImageTk
 from scrolledcanvas import ScrolledCanvas
@@ -25,15 +26,42 @@ class Register(ScrolledCanvas):
         self._y = 0
 
     def append(self, *, image=None, name=''):
-        self.create_image(self._x, self._y, image=image, anchor=tk.NW)
-        self.create_text(self._x + SIZE[0], self._y, text=name, anchor=tk.NW)
+        image_id = self.create_image(
+            self._x, self._y, image=image, anchor=tk.NW)
+        name_id = self.create_text(
+            self._x + SIZE[0], self._y, text=name, anchor=tk.NW)
         self._y += SIZE[1]
+        return image_id, name_id
 
+ExerCash = namedtuple('ExerCash', "index image name image_id name_id")
+
+
+class RegisterCash(Register):
+    def __init__(self, owner, **kwargs):
+        super().__init__(owner, **kwargs)
+        self.index = 0
+        self.exercises: list[ExerCash] = []
+
+    def refresh(self):
+        for item in self.find_all():
+            if self.type(item) in ('image', 'text'):
+                self.delete(item)
+        # for exer_cash in self.exercises:
+        #     self.delete(exer_cash.image_id)
+        #     self.delete(exer_cash.name_id)
+        for exer_cash in self.exercises:
+            super().append(image=exer_cash.image, name=exer_cash.name)
+
+    def append(self, *, image=None, name=''):
+        image_id, name_id = super().append(image=image, name=name)
+        self.exercises.append(ExerCash(
+            self.index, image, name, image_id, name_id))
+        self.index += 1
 
 class RegisterFrame(tk.Frame):
     def __init__(self, owner):
         super().__init__(owner)
-        canvas = Register(self)
+        canvas = RegisterCash(self)
         canvas.grid(column=0, row=0, sticky=tk.NSEW)
         for name, img in dirx.items():
             saved_photos.append(ImageTk.PhotoImage(img))
