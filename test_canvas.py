a3b857bd85b6517a0a771d0f7e2d55d425175422
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-01-22 10:25:51 +0200

    Add menu template to root window

--- test_canvas.py
+++ test_canvas.py
@@ -9,6 +9,10 @@ from register import RegisterFrame
 
 if __name__ == '__main__':
     root = tk.Tk()
+    menu = tk.Menu(root, tearoff=0)
+    menu.add_command(label='Delete exercise', command=None)
+    menu.add_command(label='Add exercise', command=None)
+    root['menu'] = menu
     grid_expand(root)
     frame = RegisterFrame(root)
     frame.grid(column=0, row=0, sticky=tk.NSEW)
